56f71dc2461b5e89a1f679b3850f5e0f752fa022
Riciere Marcone <85197053+ricichien@users.noreply.github.com>
2023-06-06 21:31:07 -0300

    committing contact changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "public/hero.png";
-import Button from "@/components/Button/Button";
+import Button from "../components/Button/Button";
 
 export default function Home() {
   return (
