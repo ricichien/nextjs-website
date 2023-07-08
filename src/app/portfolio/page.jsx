a74fccc4fb74d781f8fba6432345157643ed503a
Riciere Marcone <85197053+ricichien@users.noreply.github.com>
2023-07-07 23:18:15 -0300

    Update portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 const Portfolio = () => {
-  return <div className={styles.container}>Portfolio</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.items}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/illustrations" className={styles.items}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/illustrations" className={styles.items}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
